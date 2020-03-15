e64b876b2f0a7aa9fff22f8ee9d0af18c6a3512f
git <820472580@qq.com>
2020-03-15 19:39:50 +0800

    commit

--- test/test.py
+++ test/test.py
@@ -1,1 +1,16 @@
-print "hello123"
+print "test1"
+
+import sys
+from PySide2 import QtCore
+
+
+class Test(object):
+    def __init__(self):
+        print "Test Object"
+
+    
+def update():
+    print "update"
+
+if __name__ == "__main__":
+    pass
